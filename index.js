9a0449808390b871b2f923a58782f55ae736677d
moshek <mkolodny@integralads.com>
2014-08-31 15:02:54 -0400

    tests working

--- index.js
+++ index.js
@@ -1,87 +1,62 @@
-var Promise = require('bluebird');
-var Spawner = require('promise-spawner');
+var co = require('co');
+var exec = require('co-exec');
+var thunkify = require('thunkify');
 var path = require('path');
 var mkdirp = require('mkdirp');
 var fs = require('fs');
-var _ = require('lodash');
 
-var statAsync = Promise.promisify(fs.stat);
-var appendAsync = Promise.promisify(fs.appendFile);
-var mkdirpAsync = Promise.promisify(mkdirp);
+var stat = thunkify(fs.stat);
+var append = thunkify(fs.appendFile);
+var mkdirp = thunkify(mkdirp);
 
-function getGit(workingDir) {
-	return function(cmd) {
-		return new Promise(function(res, rej) {
-			var spawner = new Spawner({out: '', err: ''});
-			spawner.spawn('git ' + cmd).then(function(code) {
-				res(this.data.out[0]);
-			}, function(code) {
-				var reason = new Error(this.data.err[0] || this.data.out[0]);
-				reason.exitCode = code
-				rej(reason);
-			});
-		});
-	};
+var tree;
+
+function *gitTreeMaker(workingDir, tree) {
+	var stats;
+
+	workingDir = yield *ensureValidGitDir(workingDir);
+	yield *createTree(tree);
+	// yield exec('git branch -d __private_start');
 }
 
-function gitTreeMaker(tree, workingDir) {
-	return new Promise(function(resolve, reject) {
-		var git;
-		debugger;
-		workingDir = workingDir ? path.resolve(workingDir) : process.cwd();
+module.exports = gitTreeMaker;
 
-		// ensure the directory exists
-		statAsync(workingDir).catch(function() {
-			return mkdirpAsync(workingDir).then(function() {});
-		})
-		.then(function(stats) {
-			if (stats && !stats.isDirectory()) {
-				throw new Error(workingDir + " is a file");
-			}
-		})
-		.then(function() {
-			git = getGit(workingDir);
+//co(gitTreeMaker)('../tester', require('./test-tree.json'));
 
-			process.chdir(workingDir);
-			// ensure we are in a git repo
-			return new Promise(function(res) {
-				git('rev-parse')
-				.then(res)
-				.catch(function() {
-					git('init').then(res)
-				});
-			});
-		}).then(function(a) {
-			// start making tree
-			
-			createTree(tree);
-			function createTree(tree, lastBranch) {
-				return new Promise(function(res) {
-					_(tree).each(function(subTree, commit) {
-						var thenner = lastBranch ? git('checkout ' + lastBranch) : Promise.resolve();
-						thenner
-						.then(function() {
-							appendAsync(commit, commit + '\n')	
-						})
-						.then(function() {
-							return git('add .');
-						}).then(function() {
-							return git('commit -m "' + commit + '"');
-						})
-						.then(function() {
-							return git('rev-parse HEAD');
-						})
-						.then(function(commitHash) {
-							createTree(subTree, commitHash);
-						})
-						.catch(function(err) {
-							console.log(err);
-						});
-					});
-				});
-			}
-		})
-	});
+
+function *ensureValidGitDir(dir) {
+	dir = dir ? path.resolve(dir) : process.cwd();
+	try {
+		stats = yield stat(dir);
+		if (!stats.isDirectory()) {
+			throw new Error(dir + " is a file");
+		}
+	} catch (e) {
+		yield mkdirp(dir);
+	}
+	process.chdir(dir);
+	yield exec('git init');
+	return dir;
 }
 
-gitTreeMaker({1: {2: {3:{}}}, '../tester');
+function *createTree(tree, lastHash) {
+	for (var commit in tree) {
+		var subTree = tree[commit];
+		if (/\./.test(commit)) {
+			var branchName = commit.split('.')[1] || commit;
+			yield exec('git checkout -b ' + branchName + ' ' + (lastHash || ''));
+		}
+		if (Object.keys(tree).length > 1 && !/\./.test(commit)) {
+			var currentBranch = yield exec('git rev-parse --abbrev-ref HEAD');
+			if (!currentBranch) {
+				console.warn('you may lose ' + commit);
+			}
+		}
+		commit = commit.split('.')[0];
+		yield append(commit, commit + '\n');
+		yield exec('git add .');
+		yield exec('git commit -m "' + commit + '"');
+		var hash = yield exec('git rev-parse HEAD');
+		yield *createTree(subTree, hash);
+	}
+}
